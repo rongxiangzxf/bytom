e32644d4d04f8316ed202190f8d80310f331eb74
HAOYUatHZ <haoyu@protonmail.com>
2018-08-16 20:22:05 +0800

    clean

--- mining/mining.go
+++ mining/mining.go
@@ -77,48 +77,20 @@ type BlockTemplate struct {
 	ValidPayAddress bool
 
 	//btcd
-	Fees              []int64 `json:"fees,omitempty"`
-	SigOpCosts        []int64 `json:"sig_op_costs,omitempty"`
-	WitnessCommitment []byte  `json:"witness_commitment,omitempty"`
-}
-
-/*
-// BlockTemplate houses a block that has yet to be solved along with additional
-// details about the fees and the number of signature operations for each
-// transaction in the block.
-type BlockTemplate struct {
-	// Block is a block that is ready to be solved by miners.  Thus, it is
-	// completely valid with the exception of satisfying the proof-of-work
-	// requirement.
-	Block *wire.MsgBlock
-
 	// Fees contains the amount of fees each transaction in the generated
 	// template pays in base units.  Since the first transaction is the
 	// coinbase, the first entry (offset 0) will contain the negative of the
 	// sum of the fees of all other transactions.
 	Fees []int64
-
 	// SigOpCosts contains the number of signature operations each
 	// transaction in the generated template performs.
-	SigOpCosts []int64
-
-	// Height is the height at which the block template connects to the main
-	// chain.
-	Height int32
-
-	// ValidPayAddress indicates whether or not the template coinbase pays
-	// to an address or is redeemable by anyone.  See the documentation on
-	// NewBlockTemplate for details on which this can be useful to generate
-	// templates without a coinbase payment address.
-	ValidPayAddress bool
-
+	SigOpCosts []int64 `json:"sig_op_costs,omitempty"`
 	// WitnessCommitment is a commitment to the witness data (if any)
 	// within the block. This field will only be populted once segregated
 	// witness has been activated, and the block contains a transaction
 	// which has witness data.
-	WitnessCommitment []byte
+	WitnessCommitment []byte `json:"witness_commitment,omitempty"`
 }
-*/
 
 // NewBlockTemplate returns a new block template that is ready to be solved
 func NewBlockTemplate(c *protocol.Chain, txPool *protocol.TxPool, accountManager *account.Manager) (template *BlockTemplate, err error) {
